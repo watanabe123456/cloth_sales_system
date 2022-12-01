ba883dea18cf29dda8ee36d03469fb35b2a645f6
user <user@LAPTOP-HF5GLTM1>
2022-12-01 17:50:50 +0800

    Cloth Sales System

---
+++ src/main/java/com/example/cloth_sales_system/ClothSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloth_sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothSalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloth_sales_system/ClothSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloth_sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothSalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
