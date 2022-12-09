7f3a34978cdb3b1002fd54abe63a63bdb61907c2
user <user@LAPTOP-HF5GLTM1>
2022-12-09 12:32:31 +0800

    update

--- src/main/java/com/example/cloth_sales_system/entity/ClothSalesForShop.java
+++ src/main/java/com/example/cloth_sales_system/entity/ClothSalesForShop.java
@@ -5,23 +5,17 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.Table;
 
 import com.example.cloth_sales_system.vo.ClothSalesReq;
 
 @Entity
 @Table(name = "cloth_sales_for_shop")
-@IdClass(value = ClothSalesForShopId.class)
 public class ClothSalesForShop {
 
 	@Id
 	@Column(name = "product_number")
 	private Integer productNumber;
-
-	@Id
-	@Column(name = "members_number")
-	private String membersNumber;
 
 	@Column(name = "product_name")
 	private String productName;
@@ -38,10 +32,9 @@ public class ClothSalesForShop {
 	public ClothSalesForShop() {
 
 	}
-	
+
 	public ClothSalesForShop(ClothSalesReq req) {
 		this.productNumber = req.getProductNumber();
-		this.membersNumber = req.getMembersNumber();
 		this.productName = req.getProductName();
 		this.productQuantity = req.getProductQuantity();
 		this.productPrice = req.getProductPrice();
@@ -51,7 +44,6 @@ public class ClothSalesForShop {
 	public ClothSalesForShop(Integer productNumber, String membersNumber, String productName, Integer productQuantity,
 			Integer productPrice, Date purchaseDate) {
 		this.productNumber = productNumber;
-		this.membersNumber = membersNumber;
 		this.productName = productName;
 		this.productQuantity = productQuantity;
 		this.productPrice = productPrice;
@@ -64,14 +56,6 @@ public class ClothSalesForShop {
 
 	public void setProductNumber(Integer productNumber) {
 		this.productNumber = productNumber;
-	}
-
-	public String getMembersNumber() {
-		return membersNumber;
-	}
-
-	public void setMembersNumber(String membersNumber) {
-		this.membersNumber = membersNumber;
 	}
 
 	public String getProductName() {
